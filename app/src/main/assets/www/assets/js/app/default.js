538965304c8857c6af0b3b2db2d144e1a3bf0ece
Sean Narvasa <sean.narvasa@sprintshipworldwide.com>
2020-11-24 18:25:25 +0800

    Fetch conference list from web

--- app/src/main/assets/www/assets/js/app/default.js
+++ app/src/main/assets/www/assets/js/app/default.js
@@ -46,7 +46,8 @@ var userId = localStorage.getItem('id'),
     fullCurrentDateTime = currentDate + ' ' + currentTime,
     userKey = "USER" + KEY_CODE(3) + fullDate + time,
     conferenceKey = "CONF" + KEY_CODE(3) + fullDate + time,
-    defaultUserIconPlaceholder = "https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png";
+    defaultUserIconPlaceholder = "https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png",
+    noImage = "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id936182806?k=6&m=936182806&s=612x612&w=0&h=F5sh9tAuiAtEPNE1NiFZ7mH7-7cjx0q4CXOcxiziFpw=";
 
 function KEY_CODE(len, charSet) {
     charSet = charSet || 'ABCDFGHIJKLMNOPQRSTUVWXYZ'+'0123456789';
--- app/src/main/assets/www/assets/js/app/index.js
+++ app/src/main/assets/www/assets/js/app/index.js
@@ -1,3 +1,5 @@
+USER_PROGRAMMES();
+
 $("#card-new-conference").click(function() {
   MODAL('#modal-create-conference', 'open');
 });
@@ -82,4 +84,29 @@ function INSERT_CONFERENCE(conferenceKey, conferenceBanner, conferenceTitle, con
   	MODAL('#modal-create-conference', 'close');
     MODAL('#modal-progress', 'close');
 	MODAL('#modal-warning', 'open');
+}
+
+function USER_PROGRAMMES() {
+	database.ref(users + userGetKey + sub + conferences).on('child_added', function(data) {
+		var key = data.val().conference_key;
+		var banner = data.val().conference_banner;
+		var title = data.val().conference_title;
+		var eventPlace = data.val().conference_event_place;
+		var eventDate = data.val().conference_event_date;
+		var eventTime = data.val().conference_event_time;
+		var description = data.val().conference_description;
+		var category = data.val().conference_category;
+		var abstractSubmission = data.val().conference_abstract_Submission;
+		var fullPaperSubmission = data.val().conference_full_paper_submission;
+		var averagePercentageReport = data.val().conference_average_percentage_report;
+		var numberPapersAccomodated = data.val().conference_number_papers_accomodated;
+		var allowPaperApplication = data.val().conference_allow_paper_application;
+		var modeOfReview = data.val().conference_mode_of_review;
+		var dateTimeCreated = data.val().date_time_created;
+		if (key) {
+			$('#list-of-conferences').append('<div class="col s12 m6 l4 card-width"><div class="card card-border center-align gradient-45deg-indigo-purple"><div class="card-content white-text"><img class="responsive-img circle z-depth-4" style="height: 100px; width: 100px;" src="'+banner+'"/><h5 class="white-text mb-1">'+title+'</h5><p class="m-0">'+eventPlace+'</p><p class="mt-8">Creative usable interface & <br />designer @Clevision</p><a class="waves-effect waves-light btn gradient-45deg-deep-orange-orange border-round mt-7 z-depth-4">View Conference</a><div class="row mt-5"><a href="#" class="col s4"><h5 class="gradient-45deg-indigo-light-blue icon-background circle white-text z-depth-3"><i class="fab fa-behance"></i></h5><p class="white-text"><b>12.8k</b></p><p class="white-text">Followers</p></a><a href="#" class="col s4"><h5 class="icon-background circle gradient-45deg-indigo-blue white-text z-depth-3"><i class="fab fa-linkedin-in"></i></h5><p class="white-text"><b>10.1k</b></p><p class="white-text">Followers</p></a><a href="#" class="col s4"><h5 class="icon-background circle gradient-45deg-red-pink white-text z-depth-3"><i class="fab fa-pinterest-p"></i></h5><p class="white-text"><b>8.23k</b></p><p class="white-text">Followers</p></a></div></div></div></div>');
+			$('#conference-progress-spinner').css({"display":"none"}); 
+		}
+    });
+
 }
